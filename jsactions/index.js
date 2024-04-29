fe384c666be94496bcaded5b2b5537acba8482ca
nahloo-obi <naluprosper54@gmail.com>
2024-04-29 13:33:58 +0100

    github action

---
+++ jsactions/index.js
@@ -0,0 +1,42 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { execSync } = require('child_process');
+const path = require('path');
+
+
+try {
+
+(async () => {
+const getBenchmarkValue = core.getInput('benchmark');
+console.log('benchmark value:', getBenchmarkValue);
+
+
+ // Wait for a moment to ensure the Django app starts up
+ await new Promise(resolve => setTimeout(resolve, 5000));
+
+
+ // Get the process ID of the running Django application
+ const pidCommand = 'pgrep -f \'python manage.py runserver\'';
+ const pid = execSync(pidCommand).toString().trim();
+ var stringarray = pid.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } );
+ const processId = stringarray[0]
+
+ const path_to_script = path.join(__dirname, 'scripts', 'ramperformance.py');
+
+// Execute the Python script with the input value using the shell command
+const getScriptResult = execSync(`python ${path_to_script} ${processId} ${getBenchmarkValue}`).toString().trim();
+console.log('Result of ram usage hereee:', getScriptResult);
+
+core.setOutput("performanceresult", getScriptResult);
+
+
+
+process.exit(0); // Exit with success status code
+
+})();
+
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+
---
+++ jsactions/scripts/ramperformance.py
@@ -0,0 +1,43 @@
+import psutil
+import sys
+
+def get_ram_usage():
+    try:
+        #while not stop_signal.is_set():
+        #get script arguments from javascript
+        script_first_value = sys.argv[1]
+        BENCHMARK_VALUE = sys.argv[2]
+                
+        # # Get process object
+        application_process_ID = int(script_first_value)
+        
+        operation = psutil.Process(application_process_ID)
+
+        # # Get memory usage
+        info_mem = operation.memory_info()
+        usage_mem_mb = info_mem.rss / 1024 / 1024
+
+
+
+        return usage_mem_mb
+        #return ram_usage_list
+        
+        # if usage_mem_mb > benchmarkValue:
+        #     raise ValueError(f"Analysis failed.")
+        # else:
+        #     return usage_mem_mb
+        
+
+    except Exception as e:
+        raise ValueError(f"Process has discontinued.")
+    
+
+
+    
+if __name__ == "__main__":
+
+    ram_usage = get_ram_usage()
+    print(ram_usage)
+
+
+
